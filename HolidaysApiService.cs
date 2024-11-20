a9de2278839eb9fa0677b229a9138d75784568d4
jitani04 <119626934+jitani04@users.noreply.github.com>
2024-11-20 13:31:39 -0800

    Add files via upload

---
+++ HolidaysApiService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Lab3.Models;
+
+namespace Lab3
+{
+    public class HolidaysApiService : IHolidaysApiService
+    {
+        private static readonly HttpClient client;
+        static HolidaysApiService()
+        {
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://date.nager.at")
+            };
+        }
+        public async Task<List<HolidayModel>> GetHolidays(string countryCode, int year)
+        {
+            var url = string.Format("/api/v2/PublicHolidays/{0}/{1}", year,
+           countryCode);
+            var result = new List<HolidayModel>();
+            var response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var stringResponse = await response.Content.ReadAsStringAsync();
+                result =
+               JsonSerializer.Deserialize<List<HolidayModel>>(stringResponse,
+                new JsonSerializerOptions()
+                {
+                    PropertyNamingPolicy =
+               JsonNamingPolicy.CamelCase
+                });
+            }
+            return result;
+        }
+
+    }
+
+}
---
+++ IHolidaysApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Lab3.Models;
+namespace Lab3
+{
+
+    public interface IHolidaysApiService
+    {
+        Task<List<HolidayModel>> GetHolidays(string countryCode, int year);
+    }
+
+}
+
+
